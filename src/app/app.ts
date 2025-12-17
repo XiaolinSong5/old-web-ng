b076a72548c7010f622de90e9cd980dc40f7efad
Xiaolin <xiaolin.song@benerail.com>
2025-12-17 17:54:29 +0000

    signal form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Product} from './model/product';
+import {form, Field, submit} from '@angular/forms/signals'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Field],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('old-web-ng');
+    productModel = signal<Product>({
+      name: '',
+      qty: '',
+      price: 0.0
+    });
+    productForm = form(this.productModel);
+  onSubmit(event: Event) {
+    event.preventDefault()
+    submit(this.productForm, async (form) => {
+      const a = this.productModel;
+
+      console.log('form data name: ', a().name );
+    })
+  }
 }
---
+++ src/app/model/product.ts
@@ -0,0 +1,5 @@
+export interface Product {
+    name: string;
+    qty: string;
+    price: number;
+}
